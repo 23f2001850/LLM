14758a2ca2ce747036bbc5b14a80627f162cf11f
Krishna Jain <krishnajain2705@gmail.com>
2025-11-27 19:14:23 +0530

    Fix Chromium for Render: add single-process mode and remove --with-deps

--- backend/solver/browser.py
+++ backend/solver/browser.py
@@ -29,7 +29,24 @@ class BrowserManager:
                     '--no-sandbox',
                     '--disable-setuid-sandbox',
                     '--disable-dev-shm-usage',
-                    '--disable-gpu'
+                    '--disable-gpu',
+                    '--disable-software-rasterizer',
+                    '--disable-extensions',
+                    '--disable-background-networking',
+                    '--disable-background-timer-throttling',
+                    '--disable-backgrounding-occluded-windows',
+                    '--disable-breakpad',
+                    '--disable-component-extensions-with-background-pages',
+                    '--disable-features=TranslateUI,BlinkGenPropertyTrees',
+                    '--disable-ipc-flooding-protection',
+                    '--disable-renderer-backgrounding',
+                    '--enable-features=NetworkService,NetworkServiceInProcess',
+                    '--force-color-profile=srgb',
+                    '--hide-scrollbars',
+                    '--metrics-recording-only',
+                    '--mute-audio',
+                    '--no-first-run',
+                    '--single-process'
                 ]
             )
             self.context = await self.browser.new_context(
